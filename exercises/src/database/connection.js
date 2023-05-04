701d541d04e77ae357cb8c4dff4dbddc5ab7e811
alaaahmade <alaaahmade66@gmail.com>
2023-05-04 15:27:02 +0300

    add files

--- exercises/src/database/connection.js
+++
@@ -1,7 +0,0 @@
-// Create Connection Here
-import mongoose from 'mongoose';
-import {} from 'dotenv/config';
-
-const connection = () =>  mongoose.connect(process.env.DB_URI);
-
-export default connection;
